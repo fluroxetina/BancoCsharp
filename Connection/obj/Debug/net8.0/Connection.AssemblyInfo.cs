02f4b5e655cdf9e672c301a6e57ef1fe4dd3e39c
ArthurGuedes <arthurguedes0206@gmail.com>
2025-03-05 13:09:18 -0400

    05/03

--- Connection/obj/Debug/net8.0/Connection.AssemblyInfo.cs
+++ Connection/obj/Debug/net8.0/Connection.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Connection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc7ec505ac3522d2d063ad74f326cd5c8e08d0a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Connection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Connection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
